e939630a9b3acd15ec337fd32504336aa2b82e69
boyer <asboyer@gmail.com>
2021-02-03 21:15:32 -0500

    automated commit --updating repository

--- commit.bat
+++ commit.bat
@@ -2,6 +2,6 @@
 echo adding changes...
 git add .
 echo commiting changes...
-git commit -m "automated commit"
+git commit -m "automated commit --updating repository"
 echo pushing changes..
 git push origin master
