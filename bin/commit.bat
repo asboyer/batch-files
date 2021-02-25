9bbb6ff7c002eb52ae8281c8aa25ba27b4605356
boyer <asboyer@gmail.com>
2021-02-24 20:06:04 -0500

    test

--- bin/commit.bat
+++ bin/commit.bat
@@ -1,8 +1,9 @@
 @echo off
+set message=%1
 echo adding changes...
 git add .
 echo commiting changes...
-git commit -m "automated commit --updating repository"
+git commit -m "%1"
 echo pushing changes..
 git push origin master
 
