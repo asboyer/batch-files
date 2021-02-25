f1c751fe4fbaae94dfa373185441c422e1cf5b7c
boyer <asboyer@gmail.com>
2021-02-24 20:20:13 -0500

    automated commit --updating repository

--- bin/commit.bat
+++ bin/commit.bat
@@ -1,10 +1,12 @@
 @echo off
 set message=%1
-
+set branch=%2
+set remote=%3
 echo adding changes...
 git add .
 echo commiting changes...
 if %1.==. (git commit -m "automated commit --updating repository") else (git commit -m %message%)
 echo pushing changes..
-git push origin master
-
+if %2.==. (set branch="master")
+if %3.==. (set remote="origin")
+git push %remote% %branch%
