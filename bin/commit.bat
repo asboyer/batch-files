d76858b9b26b87647519884f793b6eb0d0f1496e
boyer <asboyer@gmail.com>
2021-07-16 15:48:08 -0400

    edited commit script

--- bin/commit.bat
+++ bin/commit.bat
@@ -1,12 +1,14 @@
 @echo off
 set message=%1
-set branch=%2
-set remote=%3
+set add=%2
+set branch=%3
+set remote=%4
 echo adding changes...
-git add .
+if %2.==. (set add=".")
+git add %add%
 echo commiting changes...
 if %1.==. (git commit -m "automated commit --updating repository") else (git commit -m %message%)
 echo pushing changes..
-if %2.==. (set branch="master")
-if %3.==. (set remote="origin")
+if %3.==. (set branch="master")
+if %4.==. (set remote="origin")
 git push %remote% %branch%
