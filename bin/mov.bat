8ce4b5d86da9c72697110f4bf9f9320321fe88b0
boyer <asboyer@gmail.com>
2021-08-01 02:08:13 -0400

    updated path

--- bin/mov.bat
+++ bin/mov.bat
@@ -1,4 +1,4 @@
 @echo off
 set movie=%1
 rename C:\Users\baller\Downloads\*.jpg %movie%.jpg
-move C:\Users\baller\Downloads\%movie%.jpg C:\Users\baller\cs\ghpage\img\movies\
+move C:\Users\baller\Downloads\%movie%.jpg C:\Users\baller\cs\asboyer\static\img\movies\
