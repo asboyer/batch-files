bdfff1fca2ea26cd22f3c09e69aad1a48fec3085
boyer <asboyer@gmail.com>
2022-06-19 16:09:13 -0400

    updated scripts

--- bin/create.bat
+++ bin/create.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set repo=%1
+set repo_name=%1
 set key=%2
 
 set /p key_file_name=<C:\Users\%USERNAME%\cs\bat\secret
@@ -10,7 +10,9 @@ python C:\Users\%USERNAME%\security_scripts\decrypt.py %key%
 set /p user=<C:\Users\%USERNAME%\Documents\.credentials\github_username.txt
 set /p gh_key=<C:\Users\%USERNAME%\Documents\.credentials\%key_file_name%
 
-git clone "https://%gh_key%@github.com/%user%/%repo%.git"
+python C:\Users\%USERNAME%\cs\automation_scripts\create.py %user% %gh_key% %repo_name%
+
+git clone "https://%gh_key%@github.com/%user%/%repo_name%.git"
 cd %repo_name%
 
 python C:\Users\%USERNAME%\security_scripts\encrypt.py %key%
